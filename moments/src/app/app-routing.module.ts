95cd7b3b2c97faecaca75972fbba3cc10692550b
unknown <renanrsa@gmail.com>
2022-11-23 17:20:51 -0300

    SubirProjeto

---
+++ moments/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about',component:AboutComponent},
+  {path: 'moments/new',component: NewMomentsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ moments/src/app/components/pages/new-moments/new-moments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moments',
+  templateUrl: './new-moments.component.html',
+  styleUrls: ['./new-moments.component.css']
+})
+export class NewMomentsComponent implements OnInit {
+btnText = "Compartilhar";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
