19b9120f4df236edfcaa9caef6cb5fad1a221fcb
Renan Araujo <renanrsa@gmail.com>
2022-11-24 20:08:59 -0300

    InserindoDados

--- moments/src/app/components/pages/new-moments/new-moments.component.ts
+++ moments/src/app/components/pages/new-moments/new-moments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moments',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewMomentsComponent implements OnInit {
 btnText = "Compartilhar";
-  constructor() { }
+
+  constructor(private momentService: MomentService) {
+
+   }
 
   ngOnInit(): void {
+  }
+
+  async createHandler(moment: Moment){
+    const formData = new FormData();
+    formData.append('title',moment.title);
+    formData.append('description',moment.description);
+    if(moment.image){
+      formData.append('image',moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
   }
 
 }
