1077888dd5a2a2f4c3d80ff211e4843f61ca4270
unknown <renanrsa@gmail.com>
2022-11-26 22:39:59 -0300

    Message

--- moments/src/app/components/pages/new-moments/new-moments.component.ts
+++ moments/src/app/components/pages/new-moments/new-moments.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
-
+import { MessagesService } from 'src/app/services/messages.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-new-moments',
   templateUrl: './new-moments.component.html',
@@ -10,7 +11,10 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentsComponent implements OnInit {
 btnText = "Compartilhar";
 
-  constructor(private momentService: MomentService) {
+  constructor(private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+    ) {
 
    }
 
@@ -26,6 +30,10 @@ btnText = "Compartilhar";
     }
 
     await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add('Momento adicionado com sucesso!');
+
+    this.router.navigate(['/']);
   }
 
 }
